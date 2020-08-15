ec7fafb74bb1760dfd13592edd72be3d655453f4
Stewie410 <stewie410@gmail.com>
2020-08-15 13:11:23 -0400

    Patch: Combo

--- dwm.c
+++ dwm.c
@@ -241,6 +241,11 @@ static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
 
+static void keyrelease(XEvent *e);
+static void combotag(const Arg *arg);
+static void comboview(const Arg *arg);
+
+
 /* variables */
 static const char broken[] = "broken";
 static char stext[256];
@@ -252,6 +257,7 @@ static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
 static void (*handler[LASTEvent]) (XEvent *) = {
 	[ButtonPress] = buttonpress,
+	[ButtonRelease] = keyrelease,
 	[ClientMessage] = clientmessage,
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
@@ -259,6 +265,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[EnterNotify] = enternotify,
 	[Expose] = expose,
 	[FocusIn] = focusin,
+	[KeyRelease] = keyrelease,
 	[KeyPress] = keypress,
 	[MappingNotify] = mappingnotify,
 	[MapRequest] = maprequest,
@@ -282,6 +289,42 @@ static Window root, wmcheckwin;
 struct NumTags { char limitexceeded[LENGTH(tags) > 31 ? -1 : 1]; };
 
 /* function implementations */
+static int combo = 0;
+
+void
+keyrelease(XEvent *e) {
+	combo = 0;
+}
+
+void
+combotag(const Arg *arg) {
+	if(selmon->sel && arg->ui & TAGMASK) {
+		if (combo) {
+			selmon->sel->tags |= arg->ui & TAGMASK;
+		} else {
+			combo = 1;
+			selmon->sel->tags = arg->ui & TAGMASK;
+		}
+		focus(NULL);
+		arrange(selmon);
+	}
+}
+
+void
+comboview(const Arg *arg) {
+	unsigned newtags = arg->ui & TAGMASK;
+	if (combo) {
+		selmon->tagset[selmon->seltags] |= newtags;
+	} else {
+		selmon->seltags ^= 1;	/*toggle tagset*/
+		combo = 1;
+		if (newtags)
+			selmon->tagset[selmon->seltags] = newtags;
+	}
+	focus(NULL);
+	arrange(selmon);
+}
+
 void
 applyrules(Client *c)
 {
@@ -1733,34 +1776,6 @@ tagmon(const Arg *arg)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
 }
-
-//void
-//tile(Monitor *m)
-//{
-//    unsigned int i, n, h, mw, my, ty;
-//	Client *c;
-//
-//	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-//	if (n == 0)
-//		return;
-//
-//	if (n > m->nmaster)
-//        mw = m->nmaster ? m->ww * m->mfact : 0;
-//	else
-//        mw = m->ww;
-//    for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-//		if (i < m->nmaster) {
-//			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-//            resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
-//			if (my + HEIGHT(c) < m->wh)
-//				my += HEIGHT(c);
-//		} else {
-//			h = (m->wh - ty) / (n - i);
-//			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
-//			if (ty + HEIGHT(c) < m->wh)
-//				ty += HEIGHT(c);
-//		}
-//}
 
 void
 togglebar(const Arg *arg)
